96c666350cfe0a5dd01b532411f0f68cb163cb31
disco07 <koneenok@outlook.fr>
2022-08-08 21:03:49 +0200

    finish

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Attestation;
 use App\Form\FormationType;
+use App\Repository\AttestationRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,10 +16,17 @@ class HomeController extends AbstractController
     /**
      * @Route("/drissa", name="app_home")
      */
-    public function index(): Response
+    public function index(Request $request, ManagerRegistry $doctrine): Response
     {
+        $entityManager = $doctrine->getManager();
         $attestion = new Attestation();
         $form = $this->createForm(FormationType::class, $attestion);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $attestion = $form->getData();
+            $entityManager->persist($attestion);
+            $entityManager->flush();
+        }
 
         return $this->render('home/index.html.twig', [
             'form' => $form->createView(),
--- src/Form/FormationType.php
+++ src/Form/FormationType.php
@@ -7,6 +7,7 @@ use App\Entity\Convention;
 use App\Entity\Etudiant;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,13 +21,9 @@ class FormationType extends AbstractType
             ->add('etudiant', EntityType::class, [
                 // looks for choices from this entity
                 'class' => Etudiant::class,
-
                 // uses the User.username property as the visible option string
                 'choice_label' => function (Etudiant $etudiant) {
                     return $etudiant->getNom() . ' ' . $etudiant->getPrenom();
-
-                    // or better, move this logic to Customer, and return:
-                    // return $customer->getFullname();
                 },
 
                 // used to render a select box, check boxes or radios
@@ -34,15 +31,8 @@ class FormationType extends AbstractType
                 // 'expanded' => true,
             ])
             ->add('convention', EntityType::class, [
-                // looks for choices from this entity
                 'class' => Convention::class,
-
-                // uses the User.username property as the visible option string
                 'choice_label' => 'nom',
-
-                // used to render a select box, check boxes or radios
-                // 'multiple' => true,
-                // 'expanded' => true,
             ])
             ->add('message', TextareaType::class)
         ;
